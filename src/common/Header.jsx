f45c2477dd3f06549c885c8f0c13c3de42685552
aadityavishnoi <vishnoiaaditya29@gmail.com>
2025-10-15 16:17:32 +0530

    commit

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -44,7 +44,7 @@ export default function Header() {
         <div className={`${menuOpen ? "block" : "hidden"} w-full md:block md:w-auto`}>
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white">
             <li><a href="/" className="font-outfit block py-2 px-3 text-blue-700">Home</a></li>
-            {/*<li><a href="/" className="font-outfit block py-2 px-3 hover:text-blue-700">Carriers</a></li>*/}
+            <li><a href="/service" className="font-outfit block py-2 px-3 hover:text-blue-700">Services</a></li>
             <li><a href="/pricing" className="font-outfit block py-2 px-3 hover:text-blue-700">Pricing</a></li>
             <li><a href="/contact" className="font-outfit block py-2 px-3 hover:text-blue-700">Contact</a></li>
           </ul>
